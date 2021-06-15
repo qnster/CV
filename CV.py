97e0b184e133bc831268d5f95630f2c8006588c7
Romeo <63035043+qnster@users.noreply.github.com>
2021-06-15 14:35:06 +0700

    Add files via upload

---
+++ CV.py
@@ -0,0 +1,80 @@
+                    '''______________________RESUME______________________'''
+   '''Highly motivated to get knowledge and skills in different areas of IT industries.  '''
+
+class Resume_of_Junior_Developer():
+
+    Name = "Roman"
+    Surname = "Lapenko"
+    Date_of_Birth = "05.09.1996"
+    Nationality = "ukrainian"
+    Location = "Kiev, Ukraine"
+    Able_for_interview_in_English = True
+    Start_working_immediately = True
+
+    def Contacts (self):
+        Phone_number = "+38 096 114 82 33"
+        Email = "lromacoo@gmail.com"
+        Facebook = "roman.lapenko.7"
+
+    def Education (self):
+        University = "NATIONAL UNIVERSITY OF FOOD TECHNOLOGIES"
+        Location = "Kiev, Ukraine"
+        Major = "biotechnology"
+        Level = "Bachelor (BA)"
+        Period = 2013-2017
+
+
+    def Education_2 (self):
+        College = "KUNMING COLLEGE OF EASTERN LANGUAGES"
+        Location = "Kunming, China"
+        Major = "Spoken Chinese and culture"
+        Level = "Basic"
+        Period = 2019-2020
+
+    def Languages (self):
+        English = "Advanced C1", "IELTS BAND 7"
+        Chinese = "Basic"
+        Ukrainian = "Native speaker"
+        Russian = "Native speaker"
+
+    def Skills (self):
+        Programming_languages = ["Python", "HTML", "CSS", "SQL"]
+        Level = "Basic"
+        Frameworks = ["Django", "Pandas", "Matplotlib", "NumPy"]
+        Level = "Basic"
+        Version_contro = "GitHub"
+        Level = "Basic"
+        MS_office = [Exel, Word, Powerpoint, Publisher]
+        Level = "Confident user"
+        Engineering_programs = [Compas, mathcad]
+        Level = "Basic"
+
+    def Courses (self):
+        '''www.kaggle.com'''
+        Basic_Python_Cource = "Completed"
+        Basic_Data_Exploration = "Completed"
+        Intermediate_Machine_Learning = "Completed"
+        Pandas = "Completed"
+        Intro_to_SQL = "Completed"
+        Into_to_big_DATA = "Completed"
+
+    def Personality (self):
+        Fast_learner = 101%
+        Accuracy = 99%
+        Responsible = 99%
+        Communication_skills = 99%
+        Team_worker = 99%
+        Adaptability = 99%
+        Creativity = math.inf
+
+    def Other (self):
+        Interests = [Science, Languages, Traveling, DJ, Sport]
+        Married = True
+        Bad_habits = False
+        Lifestyle = "Optimist"
+
+
+
+
+
+
